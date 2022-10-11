3dbe60c40bda05b1bc675f43e5d145d2ecf49a9d
hjpvandijk <hugojpvandijk@gmail.com>
2022-10-11 14:59:08 +0200

    Get epochdata in files from command line

---
+++ jupyter/getlogsfromtrainjobs.py
@@ -0,0 +1,25 @@
+import os
+
+# ret = os.popen("kubectl get configmap -n test --sort-by=.metadata.creationTimestamp --no-headers -o custom-columns=:.metadata.name").read()
+
+ret = os.popen("kubectl get pods -n test --sort-by=.metadata.creationTimestamp --no-headers -o custom-columns=:.metadata.name").read()
+
+
+jobs = ret[ret.find("fl-server")+len("fl-server")+1:]
+
+splitted = jobs.split('\n')
+
+isExist = os.path.exists("epochdata")
+if not isExist:
+    os.mkdir("epochdata")
+
+for i in splitted:
+    if "master" in i:
+        
+        retlogs = os.popen("kubectl logs -n test " + i).read()
+        epochdata = retlogs[retlogs.find("[EpochData"):]
+        f = open("epochdata/" + i + "_epochdata.txt", "a")
+        f.write(epochdata)
+        f.close()
+
+
